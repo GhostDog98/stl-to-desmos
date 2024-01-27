b2ffc3bcd2b663bce4fc924f599f2b5c6bcd6c4d
GhostDog98 <46546505+GhostDog98@users.noreply.github.com>
2024-01-27 21:09:15 +1100

    Improved dummy generation python script

--- dummythick.py
+++ dummythick.py
@@ -1,54 +1,33 @@
 import random
 
-def generate_dummy_stl(num_triangles=100):
-    stl_content = f"solid DummySTL\n"
+def format_vertex(value):
+    return format(value, ".6e").replace('e+', 'e-') if random.choice([True, False]) else format(value, ".6e")
 
-    for _ in range(num_triangles):
-        def format_vertex(value):
-            if random.choice([True, False]):  # 50/50 chance of having a negative exponent
-                return format(value, ".6e").replace('e+', 'e-')
-            else:
-                return format(value, ".6e").replace('e-', 'e+')
+def generate_vertex():
+    return tuple(format_vertex(random.uniform(0, 1e5)) for _ in range(3))
 
-        vertex1 = (
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5))
-        )
-        vertex2 = (
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5))
-        )
-        vertex3 = (
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5)),
-            format_vertex(random.uniform(0, 1e5))
-        )
 
-        stl_content += (
-            f"  facet normal 0 0 0\n"
-            f"    outer loop\n"
-            f"      vertex {vertex1[0]} {vertex1[1]} {vertex1[2]}\n"
-            f"      vertex {vertex2[0]} {vertex2[1]} {vertex2[2]}\n"
-            f"      vertex {vertex3[0]} {vertex3[1]} {vertex3[2]}\n"
-            f"    endloop\n"
-            f"  endfacet\n"
-        )
+num_triangles_to_generate = 50
+output_file_path = "dummy.stl"
+stl_content = "solid DummySTL\n"
 
-    stl_content += f"endsolid DummySTL\n"
+for _ in range(num_triangles_to_generate):
+    vertices = [generate_vertex() for _ in range(3)]
 
-    return stl_content
+    stl_content += ''.join([
+        f"  facet normal 0 0 0\n"
+        f"    outer loop\n"
+        f"      vertex {' '.join(map(str, vertex))}\n" for vertex in vertices
+    ])
+    stl_content += "    endloop\n  endfacet\n"
 
-def save_stl_file(file_path, stl_content):
-    with open(file_path, 'w') as f:
-        f.write(stl_content)
+# Repetition of stl_content (1000 times)
+num_repititions = 40000
+stl_content *= num_repititions
 
-if __name__ == "__main__":
-    num_triangles_to_generate = 100
-    output_file_path = "dummy.stl"
+stl_content += f"endsolid DummySTL\n"
 
-    dummy_stl_content = generate_dummy_stl(num_triangles_to_generate)
-    save_stl_file(output_file_path, dummy_stl_content)
+with open(output_file_path, 'w') as f:
+    f.write(stl_content)
 
-    print(f"Dummy STL file '{output_file_path}' generated with {num_triangles_to_generate} triangles.")
+print(f"Dummy STL file '{output_file_path}' generated with {num_triangles_to_generate + num_repititions} triangles.")
